b825e64e5a412862d3b520efeb6ef3d270e6abda
Jonathan McCormack <jonathan.mccormack@fcc.gov>
2018-08-17 17:23:53 -0400

    fixed error handling for empty output or incorrect sort_columns

--- mba2mfii/tasks.py
+++ mba2mfii/tasks.py
@@ -11,55 +11,61 @@ import pandas as pd
 
 
 class Task(object):
-    
+
     def __init__(self):
         import pandas as pd
         import logging
-        
+
         self.input  =   []
         self.output =   None
-        
+
         self.args   =   {   'clobber':      False,
                             'dry_run':      False,
                             'device_imei':  None    }
-        
+
         self.data   =   pd.DataFrame()
-                
-        self.logger =   logging.getLogger(__name__)    
-    
-    
+
+        self.logger =   logging.getLogger(__name__)
+
+
     def build_output(self, df):
         """
         Iteratively build output by appending pandas DataFrames
         """
         import pandas as pd
-        
+
         if not isinstance(df, pd.DataFrame):
             msg = 'argument df must be a pandas DataFrame:{}'.format(type(df))
             self.logger.error(msg)
             raise ValueError(msg)
-        
+
         if df.empty:
-            self.logger.warn('detected empty DataFrame, skipping build_output()')
+            self.logger.warn('detected empty DataFrame -- skipping build_output()')
         else:
             self.logger.debug('appending {} rows to output DataFrame'.format(len(df)))
             self.data = self.data.append(df, ignore_index=True)
-    
-    
+
+
     def sort_output(self, sort_columns=None, ascending=True):
         """
         Sort data values by sort_columns
         """
-        if sort_columns:
-            self.data = self.data.sort_values(by=sort_columns, ascending=ascending)
-    
-    
+        if self.data.empty:
+            self.logger.warn(   'skipping sort of output -- results DataFrame empty' )
+        else:
+            if (set(sort_columns) - set(self.data.columns)):
+                self.logger.error(  'skipping sort of output -- sort_columns:%s not in DataFrame columns:%s'
+                                    (set(sort_columns) - set(self.data.columns)), self.data.columns     )
+            else:
+                self.data = self.data.sort_values(by=sort_columns, ascending=ascending)
+
+
     def write_output(self, output=None):
         """
         Write combined pandas DataFrame to output CSV
         """
         import os
-        
+
         if output is None:
             output = self.output
         
@@ -69,7 +75,7 @@ class Task(object):
                     os.makedirs(os.path.dirname(output))
                 except:
                     pass
-        
+
         if self.data.empty:
             self.logger.warn('skipping write to output file:{} -- results DataFrame empty'.format(output))
         else:
